27286f25655746bd877b8176cbc891942f7a7763
Thompson <thompson.msds@gmail.com>
2022-09-04 17:21:28 -0300

    add: socket

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,18 @@
 import 'dotenv/config'
 
 import express from 'express'
-import cors from 'cors'
+import cors from "cors"
+import http from "http"
+import { Server } from "socket.io"
 
 import { connectToDatabase } from "./database/service"
 
 import { routes } from './routes';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.json());
 
 app.use(cors({
@@ -16,8 +21,12 @@ app.use(cors({
 
 connectToDatabase().then(() => {
 
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+    });
+
     routes(app);
-    app.listen(4000, () => console.log('Server started in port 4000'));
+    server.listen(4000, () => console.log('Server started in port 4000'));
 
 }).catch((error: Error) => {
     console.error("Database connection failed", error);
