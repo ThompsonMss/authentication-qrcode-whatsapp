62ed8f7ee95a03cc4f5a5ec744a2eb190fd83763
Thompson <thompson.msds@gmail.com>
2022-09-07 15:38:51 -0300

    alter cors in bacnkend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,9 +15,21 @@ const io = new Server(server);
 
 app.use(express.json());
 
-app.use(cors({
-    origin: '*'
-}));
+const whitelist = ["http://localhost:5173", "0.0.0.0"]
+
+const corsOptions = {
+    origin: function (origin: any, callback: any) {
+        console.log(origin)
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
 
 connectToDatabase().then(() => {
 
